481e9ee35f4b473ad3a9937e8ffa86573235f593
chabeerabsal <chabeerabsal@gmail.com>
2025-06-19 23:03:37 +0530

    parking lot

--- src/Service/ParkingService.java
+++ src/Service/ParkingService.java
@@ -1,10 +1,39 @@
 package Service;
 
-import Modeller.Vehicle;
+import Modeller.*;
+
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.*;
+import java.util.Map;
 
 public class ParkingService {
-    public void parkVehicle(Vehicle vehicle)
+    private Map<VehicleType, List<Slot>> parkingSlots = new HashMap<VehicleType,List<Slot>>();
+    private int ticketCounter=0;
+    public Ticket parkVehicle(Vehicle vehicle)
     {
+         VehicleType type= vehicle.getVechicletype();
+         List<Slot> slotList = parkingSlots.get(type);
+         if(slotList == null)
+         {
+             System.out.println("Slot Type Not Found");
+             return null;
+         }
+         for (Slot slot : slotList) {
+             if(slot.getStatus()== SlotStatus.AVAILABLE)
+             {
+              slot.markOccupied();
+                 String ticketNumber = "T" + ticketCounter++;
+                 Ticket ticket = new Ticket(ticketNumber, vehicle, slot, LocalDateTime.now());
+
+                 // 5. Store ticket
+                 activeTickets.put(ticketNumber, ticket);
+
+                 return ticket;
+             }
+         }
+
+
 
     }
 }
